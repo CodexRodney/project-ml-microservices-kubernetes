89d480390e8ba9c2b58b413b5b7a1a30ad4f5193
CodexRodney <rodney002mwanje@gmail.com>
2023-03-12 11:56:37 +0300

    Added: run kubernetes script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+export dockerpath=codexrodney/udacity:v1.0.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run machine-learning\
+    --image=$dockerpath\
+    --port=80 --labels app=machine-learning
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward pod/machine-learning-5c7c5fcd7c-m4n9x --address 0.0.0.0 8000:80
 
