72e1ad1ea397aaf720b880e836cc008dbcd69f00
CodexRodney <rodney002mwanje@gmail.com>
2023-03-12 11:57:07 +0300

    Added: script to upload docker image

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+docker tag udacity codexrodney/udacity:v1.0.0
+export dockerpath=codexrodney/udacity:v1.0.0
 
 # Step 2:  
 # Authenticate & tag
+docker login
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
